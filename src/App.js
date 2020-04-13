396cde89dbf3bba77771a91b644c6d30198f7e9e
shaina25 <shrqmohd@gmail.com>
2020-04-13 22:30:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>WHAT IS AUTISM ?</h1>
+      <img src={process.env.PUBLIC_URL+"1.jpg"} />
+      <h2>SOME FREQUENTLY ASKED QUESTIONS</h2>
+      <h3>Q. WHAT ARE AUTISM SPECTRUM DISORDERS ?</h3>
+      <h4>Autism spectrum disorders (ASDs) represent a range of brain disorders that are characterized</h4>
+      <h4>by restricted patterns of behavior and impairments in social communication and in social</h4> 
+      <h4> communication and interactions. These disorders share common origins and features, but are </h4>
+      <h4> classified as spectrum disorders because  symptoms and severity vary among individuals.</h4>
+      <h3>Q. WHAT IS AUTISTIC DISORDER ?</h3>
+      <h4>Autistic disorder, commonly referred to as autism, is the most prevalent ASD and severely</h4>
+       <h4>impairs a child’s social interaction and communication abilities.</h4>
+      <h3>Q. WHAT ARE SYMPTOMS OF AUTISM ?</h3>
+      <h4>Symptoms/behaviors of ASDs can range from mild to severe, and may seem to appear gradually </h4>
+      <h4>or suddenly. Atypical development may be observed from birth, or more commonly, become noticeable</h4>
+      <h4> during the 12- to 36-month period. Symptoms include:</h4>
+      <h4>=> 'Social Deficits' - Children with autism have difficulty in social interactions. They may avoid eye</h4>
+      <h4> contact and interactions with people and resist or passively accept attention. </h4>
+      <h4>=> 'Communication Difficulties' - Communication skills are affected in children with autism, but difficulties</h4>
+      <h4> vary. Some children may have good basic language skills, but exhibit difficulty initiating or sustaining </h4>
+      <h4>conversations, such as not giving others the opportunity to respond. </h4>
+      <h4>=> 'Repetitive Behaviors' - Patterns of behavior, interests and activities may be restricted, repetitive </h4>
+      <h4> or stereotyped.</h4>
+      <h3>Q. WHAT IS ASPERGER'S SYNDROME ?</h3>
+      <h4>Asperger's Syndrome is the second most common ASD and is a milder form of autism. Children with Asperger’s </h4>
+      <h4>Syndrome exhibit higher language development than children with autism and often have normal intellectual </h4>
+      <h4>ability combined with a disinterest in social communication. Children with Asperger’s may not initiate or</h4>
+      <h4> sustain a conversation and do not compensate for their limited language by using nonverbal means of communication,</h4>
+      <h4> thus limiting their peer relationships. Similar to autism, children with Asperger’s do not share enjoyment </h4>
+      <h4>or interests with others.</h4>
+      <h3>Q. WHAT IS RETT SYNDROME ?</h3>
+      <h4>Rett syndrome is a genetic disorder almost exclusively found in females. After early normal development, autistic </h4>
+      <h4>symptoms begin to develop between 6 and 18 months, which typically include shunning social contact, talking cessation,</h4>
+      <h4> unique motor behaviors, and a regression in skills. A single gene mutation has been identified as a cause of Rett</h4>
+      <h4> syndrome, a finding that may enable researchers to develop improved diagnostics, earlier interventions and better</h4>
+      <h4> treatments for the condition.</h4>
+      <h3>Q. WHO DEVELOPS ASDS ?</h3>
+      <h4>ASDs are usually evident by the age of 3, though diagnosis may be made as early as 12 to 18 months, and as late as 4 to 6</h4>
+      <h4> years (or later). According to the Center for Disease Control (CDC), about 1 in 59 children have an autism spectrum disorder.</h4>
+      <h4> ASDs are three to four times more common in boys than in girls. However, girls with these disorders tend to have more severe </h4>
+      <h4>symptoms and lower intelligence. Some children will need ongoing supervision, while others, with the right support, may pursue</h4>
+      <h4> higher education and fulfilling jobs. These disorders affect people of all racial, ethnic and socioeconomic groups.</h4>
+      <h4></h4>
+      <h4></h4>
+      <h4></h4>
+        
+
     </div>
   );
 }
